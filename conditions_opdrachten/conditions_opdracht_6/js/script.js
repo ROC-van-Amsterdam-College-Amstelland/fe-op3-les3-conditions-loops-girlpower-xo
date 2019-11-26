75fe2389be14edbd8ab90b346b32206e33b2209d
Iris Petersen <irispe2707@gmail.com>
2019-11-26 13:46:25 +0100

    opdracht 1 done

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+var myNumber1= prompt("geef aub een getal op");
+var myNumber2= prompt("geef aub een getal op");
+var myOperator= "-";
+var reslutaat;
+ 
+if(myOperator == "+"){
+    resultaat = myNumber1 + myNumber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+}
+    else if (myOperator == "-"){
+        resultaat = myNumber1 - myNumber2;
+        document.getElementById("resultaat").innerHTML = resultaat;
+    }
