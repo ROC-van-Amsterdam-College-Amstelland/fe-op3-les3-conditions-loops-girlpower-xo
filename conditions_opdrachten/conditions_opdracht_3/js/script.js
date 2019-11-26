0e727c3a69423a61b8e0f66bdc8c142559968389
Iris Petersen <irispe2707@gmail.com>
2019-11-26 11:31:18 +0100

    opdraht 3 done

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,22 @@
 //javascript code
+
+function checkSaldo(){
+    var geldOpnamen = document.getElementById("geldOpnamen").value;
+
+    
+
+    var bankSaldo = 500;
+    
+    if(bankSaldo == 500){
+        alert("je salaris is gestort");
+    }
+
+    if(bankSaldo > 500){
+        alert("je bonus is gestort");
+    }
+
+    if(bankSaldo < 500){
+        alert("er is geen geld gestort");
+    }
+
+}
