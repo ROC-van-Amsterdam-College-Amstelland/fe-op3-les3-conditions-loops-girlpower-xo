a60a01c00a07a76395a65db07002229a3e272777
Iris Petersen <iris.petersen@student.rocva.nl>
2019-12-04 11:22:54 +0100

    opdracht 5 loops done

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,26 @@
-//javascript code
+// Javascript code
+
+
+
+for (var teller = 1; teller <= 20; teller++) {
+    // console.log(teller);
+    //     if (teller & 1) {
+    //         console.log("oneven");
+    //     }
+    //     else {
+    //         console.log("even");
+    //     }
+    // if (teller % 2 == 0)
+    //     return true;
+    // else
+    //     return false;
+    if (teller % 2 == 0) {
+        // console.log(teller + ' is even');
+        document.getElementById("result").innerHTML += `is even ${teller} </br>`
+
+    } else {
+        console.log(teller + ' is oneven');
+
+    }
+
+} 
