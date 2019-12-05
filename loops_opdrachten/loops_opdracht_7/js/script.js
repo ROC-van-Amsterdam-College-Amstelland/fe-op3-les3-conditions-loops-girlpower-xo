f023fb7eda93fe592e55a864a9060cd08b0bd548
Iris Petersen <iris.petersen@student.rocva.nl>
2019-12-05 15:15:29 +0100

    opdracht 7 looops done

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,26 @@
 //javascript code
+
+
+
+
+for (var teller = 1; teller <= 100; teller++) {
+
+    if (teller % 3 == 0 && teller % 5 == 0) {
+        document.getElementById("result").innerHTML += `<strong>vet cool </strong><br>`;
+    }
+
+    if (teller % 3 == 0) {
+
+        document.getElementById("result").innerHTML += `<strong>vet</strong><br>`;
+    }
+    else if (teller % 5 == 0) {
+        document.getElementById("result").innerHTML += `cool  </br>`;
+    }
+
+    else {
+        document.getElementById("result").innerHTML += teller + "<br>";
+    }
+
+
+
+}
